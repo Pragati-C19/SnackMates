243c60bffb7271581b86cf1fb899a0db3a0028e9
Pragati Chothe <pragatichothe@gmail.com>
2024-08-20 01:28:13 +0530

    💡 update comments

--- routes/menus.js
+++ routes/menus.js
@@ -6,7 +6,7 @@ const { getMenus, getMenu } = require("../controller/auth-controller");
 //get all menus http://localhost:4000/menus/all
 router.get("/all", getMenus);
 
-//get specific menu http://localhost:4000/auth/login
+//get specific menu http://localhost:4000/menus/{menu_id}
 router.get("/:id", getMenu);
 
 //todo : Get Single Menu Type - Fetches details of a specific menu type. will need this after search tab
