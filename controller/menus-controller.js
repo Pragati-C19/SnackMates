bd4b1edfbfa6ee14a05a35d48e1a370e65b470b4
Pragati Chothe <pragatichothe@gmail.com>
2024-08-20 01:22:13 +0530

    menu api's are added

---
+++ controller/menus-controller.js
@@ -0,0 +1,14 @@
+//All request and response for menu API wrote here
+
+//DB Connection
+const connection = require("../database/db-connection");
+
+const getMenu = (req, res) =>{
+
+}
+
+const getMenus = (req,res) =>{
+
+}
+
+module.exports = {getMenu,getMenus}
---
+++ routes/menus.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+
+const { getMenus, getMenu } = require("../controller/auth-controller");
+
+//get all menus http://localhost:4000/menus/all
+router.get("/all", getMenus);
+
+//get specific menu http://localhost:4000/auth/login
+router.get("/:id", getMenu);
+
+//todo : Get Single Menu Type - Fetches details of a specific menu type. will need this after search tab
+
+//Export all userrouters from here
+module.exports = router;
