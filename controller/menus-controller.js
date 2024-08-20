06353aca21ca349da7d5bdbe36e879b9fd8c2249
Pragati Chothe <pragatichothe@gmail.com>
2024-08-20 20:53:04 +0530

    🐛 getMenuType is giveing menu not found error

--- controller/menus-controller.js
+++ controller/menus-controller.js
@@ -3,8 +3,8 @@
 //DB Connection
 const db = require("../database/db-connection");
 
-const getMenu = (req, res) =>{
-//Extract ID from request parameter
+const getMenu = (req, res) => {
+  //Extract ID from request parameter
   const menuId = req.params.menu_id;
 
   //sql query to find menu_id
@@ -12,35 +12,56 @@ const getMenu = (req, res) =>{
 
   db.query(getMenuQuery, [menuId], (error, results) => {
     if (error) {
-        console.error("Error while Fetching Menu by ID:", error);
-        res.status(500).json({ statusCode: 500, error: "Failed to Fetch Menu" });
+      console.error("Error while Fetching Menu by ID:", error);
+      res.status(500).json({ statusCode: 500, error: "Failed to Fetch Menu" });
     } else {
-        if (results.length === 0) {
-            res.status(404).json({ statusCode: 404, error: "Menu not found" });
-        } else {
-            res.status(200).json({ statusCode: 200, data: results[0] });
-            console.log("[INFO] Menu details of specific ID: ", results)
-        }
+      if (results.length === 0) {
+        res.status(404).json({ statusCode: 404, error: "Menu not found" });
+      } else {
+        res.status(200).json({ statusCode: 200, data: results[0] });
+        console.log("[INFO] Menu details of specific ID: ", results);
+      }
     }
-    });
-  }
+  });
+};
 
+const getMenus = (req, res) => {
+  const getMenusquery = "SELECT * FROM menu_table";
+  db.query(getMenusquery, (error, results) => {
+    if (error) {
+      console.error("Error while Fetching All Menus:", error);
+      res.status(500).json({ statusCode: 500, error: "Failed to Fetch menus" });
+    } else {
+      res.status(200).json({ statusCode: 200, data: results });
+      console.log("[INFO] All Menu List: ", results);
+    }
+  });
+};
 
-const getMenus = (req,res) =>{
-    const getMenusquery = "SELECT * FROM menu_table";
-    db.query(getMenusquery, (error, results) => {
-        if (error) {
-            console.error("Error while Fetching All Menus:", error);
-            res.status(500).json({ statusCode: 500, error: "Failed to Fetch menus" });
-        } else {
-            res.status(200).json({ statusCode: 200, data: results });
-            console.log("[INFO] All Menu List: ", results)
-        }
-    });
-}
+//todo: it's giving error menu not found
+const getMenuByType = (req, res) => {
+  //Extract Type from request parameter
+  const {menuType} = req.params;
+  console.log(`[DEBUG] Menu type received: ${menu_type}`);
 
-const getMenuByType = (req,res) =>{
+  //sql query to find menu_type
+  const getMenuByTypeQuery = "SELECT * FROM menu_table WHERE menu_type = ?";
 
-}
+  console.log("[INFO] Running Query: ", query, "with menuType:", menuType);
 
-module.exports = {getMenu,getMenus, getMenuByType}
+  db.query(getMenuByTypeQuery, [menuType], (error, results) => {
+    if (error) {
+      console.error("Error while Fetching Menu by Type:", error);
+      res.status(500).json({ statusCode: 500, error: "Failed to Fetch Menu" });
+    } else {
+        console.log("[DEBUG] Query results: ", results);
+      if (results.length === 0) {
+        res.status(404).json({ statusCode: 404, error: "Menusss not found" });
+      } else {
+        res.status(200).json({ statusCode: 200, data: results });
+      }
+    }
+  });
+};
+
+module.exports = { getMenu, getMenus, getMenuByType };
