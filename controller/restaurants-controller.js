743449eab08be3930792ce05e8c165c59b20af17
Pragati Chothe <pragatichothe@gmail.com>
2024-08-20 22:33:44 +0530

    🛂 getRestaurants API is working

--- controller/restaurants-controller.js
+++ controller/restaurants-controller.js
@@ -1,14 +1,24 @@
 //All request and response for restaurants API wrote here
 
 //DB Connection
-const connection = require("../database/db-connection");
+const db = require("../database/db-connection");
 
 const getRestaurantMenu = (req, res) =>{
 
 }
 
 const getRestaurants = (req,res) =>{
+    const getRestaurantsQuery = "SELECT * FROM restaurant_table";
 
+    db.query(getRestaurantsQuery, (error, results) => {
+      if (error) {
+        console.error("Error while fetching all restaurants:", error);
+        res.status(500).json({ statusCode: 500, error: "Failed to fetch Restaurants" });
+      } else {
+        res.status(200).json({ statusCode: 200, data: results });
+        console.log("[INFO] All Restaurants: ", results);
+      }
+    });
 }
 
 module.exports = {getRestaurantMenu,getRestaurants}
