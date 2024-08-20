6f025a128cd4a6445c6ed206f40014f2d1dc597d
Pragati Chothe <pragatichothe@gmail.com>
2024-08-20 19:09:02 +0530

    🛂 userLogin API is working

--- controller/auth-controller.js
+++ controller/auth-controller.js
@@ -37,6 +37,48 @@ const registerUser = (req,res) =>{
 }
 
 const loginUser = (req,res) =>{
+    const { emailID, password } = req.body;
+  console.log("[INFO] loginUser: email_id and Password => ", emailID, password);
+
+  // Retrieve user data from the database based on the provided email
+  const loginQuery = "SELECT * FROM user_table WHERE email = ?";
+
+  db.query(loginQuery, [emailID], (error, results) => {
+    if (error) {
+      console.error("Error While Retrieving User Data:", error);
+      res.status(500).json({ statusCode: 500, error: "Internal server error" });
+    } else {
+      if (results.length === 0) {
+        // User with the provided email not found
+        res.status(404).json({ statusCode: 404, error: "User not found" });
+      } else {
+        // User found, check password
+        const user = results[0];
+        console.log("If user found and password checking remain: ", user)
+
+        // Compare the provided password with the stored password (plaintext)
+        if (password === user.password) {
+          // Passwords match, user is authenticated
+          user.password = null
+          console.log("Passwords match, user is authenticated : ",user)
+
+          //JWT Token Code
+          const jwtToken = jwt.sign(user.user_id, process.env.JWT_SECRET);
+          res.status(200).json({
+            statusCode: 200,
+            message: "Login Successful",
+            accessToken: jwtToken,
+          });
+
+        } else {
+          // Passwords do not match : Authentication failed
+          res
+            .status(401)
+            .json({ statusCode: 401, error: "Wrong Password, Authentication Failed" });
+        }
+      }
+    }
+  });
 
 }
 
