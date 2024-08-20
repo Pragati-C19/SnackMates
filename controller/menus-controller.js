b9505931b0d96eaef07784c051d94e25fc7ac216
Pragati Chothe <pragatichothe@gmail.com>
2024-08-20 19:53:31 +0530

    🛂 getMenus API is working

--- controller/menus-controller.js
+++ controller/menus-controller.js
@@ -12,13 +12,14 @@ const getMenu = (req, res) =>{
 
   db.query(getMenuQuery, [menuId], (error, results) => {
     if (error) {
-        console.error("Error while Fetching menu by ID:", error);
+        console.error("Error while Fetching Menu by ID:", error);
         res.status(500).json({ statusCode: 500, error: "Failed to Fetch Menu" });
     } else {
         if (results.length === 0) {
             res.status(404).json({ statusCode: 404, error: "Menu not found" });
         } else {
             res.status(200).json({ statusCode: 200, data: results[0] });
+            console.log("[INFO] Menu details of specific ID: ", results)
         }
     }
     });
@@ -26,7 +27,16 @@ const getMenu = (req, res) =>{
 
 
 const getMenus = (req,res) =>{
-
+    const getMenusquery = "SELECT * FROM menu_table";
+    db.query(getMenusquery, (error, results) => {
+        if (error) {
+            console.error("Error while Fetching All Menus:", error);
+            res.status(500).json({ statusCode: 500, error: "Failed to Fetch menus" });
+        } else {
+            res.status(200).json({ statusCode: 200, data: results });
+            console.log("[INFO] All Menu List: ", results)
+        }
+    });
 }
 
 const getMenuByType = (req,res) =>{
