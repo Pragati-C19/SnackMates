6b1935f1d6693a11a116944666f28a3ab2df16a3
Pragati Chothe <pragatichothe@gmail.com>
2024-08-20 18:54:52 +0530

    🛂 registerUser API is working

--- controller/auth-controller.js
+++ controller/auth-controller.js
@@ -2,13 +2,38 @@
 
 //DB Connection
 const jwt = require("jsonwebtoken");
-const connection = require("../database/db-connection");
+const db = require("../database/db-connection");
 
 //Code is showing 'MODULE_NOT_FOUND' error and didn't give token bcoz we haven't call .env function
 require("dotenv").config();
 
 const registerUser = (req,res) =>{
-    console.log("this is a register")
+    const {
+        userID,
+        emailID,
+        userName,
+        password,
+        created_at,
+      } = req.body;
+    
+      // Insert user data into the MySQL database
+      const registerQuery =
+        "INSERT INTO user_table (user_id, email, user_name, password, created_at) VALUES (?, ?, ?, ?, NOW())";
+      db.query(
+        registerQuery,
+        [userID, emailID, userName, password, created_at],
+        (error, results) => {
+          if (error) {
+            console.error("Error While Registering User in MySQL:", error);
+            res.status(500).json({ statusCode: 500, error: "Registration Failed" });
+          } else {
+            res
+              .status(201)
+              .json({ statusCode: 201, message: "User Registered Successfully" });
+          }
+          console.log("[INFO] registerQuery: ", results);
+        }
+      );
 }
 
 const loginUser = (req,res) =>{
