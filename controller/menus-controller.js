53cafeb0b210b94fa3db8f9ec9f275367a666d63
Pragati Chothe <pragatichothe@gmail.com>
2024-08-20 19:37:45 +0530

    after sending getMenu API getting error menu not found

--- controller/menus-controller.js
+++ controller/menus-controller.js
@@ -10,7 +10,7 @@ const getMenu = (req, res) =>{
   //sql query to find menu_id
   const getMenuQuery = "SELECT * FROM menu_table WHERE menu_id = ?";
 
-  db.query(getMenuQuery, menuId, (error, results) => {
+  db.query(getMenuQuery, [menuId], (error, results) => {
     if (error) {
         console.error("Error while Fetching menu by ID:", error);
         res.status(500).json({ statusCode: 500, error: "Failed to Fetch Menu" });
