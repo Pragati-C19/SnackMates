2c9a094a5c286e06bcf3024f131d86c65d1b3e2e
Pragati Chothe <pragatichothe@gmail.com>
2024-08-21 13:22:00 +0530

    🛂 autherization added to see favorites of user

--- routes/favorites.js
+++ routes/favorites.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { authenticateToken } = require("../middleware/token-auth");
 
 const {
   getFavorites,
@@ -8,13 +9,13 @@ const {
 } = require("../controller/favorites-controller");
 
 // Get all favorites http://localhost:4000/favorites/all/{user_id}
-router.get("/all/:user_id", getFavorites);
+router.get("/all/:user_id", authenticateToken, getFavorites);
 
 // Add to favorites http://localhost:4000/favorites/{user_id}
-router.post("/:user_id", addFavorites);
+router.post("/:user_id", authenticateToken, addFavorites);
 
 // Remove to favorites http://localhost:4000/favorites/{user_id}/{favorite_id}
-router.delete("/:user_id/:favorite_id", deleteFavorite);
+router.delete("/:user_id/:favorite_id", authenticateToken, deleteFavorite);
 
 // Export all favorites routers from here
 module.exports = router;
